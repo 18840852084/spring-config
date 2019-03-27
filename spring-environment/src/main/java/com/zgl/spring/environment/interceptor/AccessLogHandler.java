5eb4e2b784f48479e1ac5559711225771ee52b29
zgl <285683264@qq.com>
2019-03-27 20:15:12 +0800

    add log4j2 configuration

--- spring-environment/src/main/java/com/zgl/spring/environment/interceptor/AccessLogHandler.java
+++ spring-environment/src/main/java/com/zgl/spring/environment/interceptor/AccessLogHandler.java
@@ -1,6 +1,8 @@
 package com.zgl.spring.environment.interceptor;
 
-import lombok.extern.slf4j.Slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,18 +10,22 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
  * @author zgl
  * @date 2019/3/27 下午4:46
  */
-//@Slf4j
+
 public class AccessLogHandler implements HandlerInterceptor {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		long currentTime = System.currentTimeMillis();
 		request.setAttribute("Current-Time", currentTime);
-		//log.info("{} {}", request.getMethod(), request.getRequestURI());
+		logger.error("{} {}", request.getMethod(), request.getRequestURI());
+		System.out.println(request.getMethod()+"========="+request.getRequestURI());
 		return true;
 	}
 
@@ -30,8 +36,7 @@ public class AccessLogHandler implements HandlerInterceptor {
 
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
-		//log.info("afterCompletion...........");
 		long currentTime = (long)request.getAttribute("Current-Time");
-		//log.info("{} has finished in {}ms", request.getRequestURI(), System.currentTimeMillis() - currentTime);
+		logger.error("{} has finished in {}ms", request.getRequestURI(), System.currentTimeMillis() - currentTime);
 	}
 }
